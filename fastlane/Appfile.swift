dc53963992faed5af38a8a8fbf1728badc82bf25
Moojong Song <purechris@me.com>
2018-04-04 19:34:46 +0900

    feat: Fastlane setup
    Setup Fastlane for test
    * if you want perform this function, enter "fastlane snapshot" on terminal.

---
+++ fastlane/Appfile.swift
@@ -0,0 +1,7 @@
+var appIdentifier: String { return "com.ChrisMJSong.SwiftSample" } // The bundle identifier of your app
+var appleID: String { return "purechris@me.com" } // Your Apple email address
+
+
+
+// For more information about the Appfile, see:
+//     https://docs.fastlane.tools/advanced/#appfile
