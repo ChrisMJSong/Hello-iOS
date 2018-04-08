5f3dae91135cea87c5b4aa124adce8607447df1b
Moojong Song <purechris@me.com>
2018-04-08 23:54:17 +0900

    feat: add override function
    add override function for description.

--- SwiftSample/SubProjects/ProjectInitNavigationViewController.swift
+++ SwiftSample/SubProjects/ProjectInitNavigationViewController.swift
@@ -21,15 +21,11 @@ class ProjectInitNavigationViewController: UINavigationController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    /// Return a string that viewcontroller description
+    ///
+    /// - Returns: description string
+    func description() -> String {
+        // it will be override
     }
-    */
 
 }
