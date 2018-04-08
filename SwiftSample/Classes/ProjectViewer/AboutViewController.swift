99dc2378531dc05978c8b49ff084f5626eeca5d4
Moojong Song <purechris@me.com>
2018-04-08 23:33:40 +0900

    feat: Add about viewcontroller
    add about view controller, it is show description about project.

---
+++ SwiftSample/Classes/ProjectViewer/AboutViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AboutViewController.swift
+//  SwiftSample
+//
+//  Created by Chris Song on 08/04/2018.
+//  Copyright © 2018 ChrisMJSong. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
