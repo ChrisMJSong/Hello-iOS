53adf720cb54e6a9119b497610faed1451ec73ee
Moojong Song <purechris@me.com>
2018-05-06 23:49:30 +0900

    feat: add OpenChatMessageTableViewCell
    add OpenChatMessageTableViewCell class

---
+++ SwiftSample/SubProjects/OpenChat/Classes/ChatView/OpenChatMessageTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  OpenChatMessageTableViewCell.swift
+//  SwiftSample
+//
+//  Created by Chris Song on 06/05/2018.
+//  Copyright © 2018 ChrisMJSong. All rights reserved.
+//
+
+import UIKit
+
+class OpenChatMessageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
